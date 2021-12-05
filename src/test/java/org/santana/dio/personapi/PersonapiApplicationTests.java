ca2420d358490115f4bfb365fd91019cae2193fb
daniloSantana <danilostna@gmail.com>
2021-12-05 18:17:17 -0300

    Comit inicial

---
+++ src/test/java/org/santana/dio/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.santana.dio.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
